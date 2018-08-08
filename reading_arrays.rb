fb2a82de3eb7bb184dddf8e25733dce7111c0938
graciemzhang <gracie.zhang@lps-students.org>
2018-08-08 18:04:03 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -35,5 +35,5 @@ end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  return STUDENTS_NAMES[0..1..2]
+  return STUDENTS_NAMES[0..2]
 end
