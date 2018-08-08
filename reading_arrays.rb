92cf02390ebaff6e531227ffc10d07477d16fc6e
graciemzhang <gracie.zhang@lps-students.org>
2018-08-08 18:06:52 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -14,26 +14,26 @@ end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
-  return STUDENTS_NAMES[3]
+  return STUDENT_NAMES[3]
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
-  return STUDENTS_NAMES[5]
+  return STUDENT_NAMES[5]
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
-  return STUDENTS_NAMES.first
+  return STUDENT_NAMES.first
   
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
-  return STUDENTS_NAMES.last
+  return STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  return STUDENTS_NAMES[0..2]
+  return STUDENT_NAMES[0..2]
 end
